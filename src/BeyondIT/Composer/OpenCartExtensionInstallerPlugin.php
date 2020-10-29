65beb139dc031da63004fa159032fe480e5e3c18
JD <zelenuj@gmail.com>
2020-10-29 16:01:20 +0200

    Вияснення як працює uninstall

--- src/BeyondIT/Composer/OpenCartExtensionInstallerPlugin.php
+++ src/BeyondIT/Composer/OpenCartExtensionInstallerPlugin.php
@@ -5,6 +5,7 @@ namespace BeyondIT\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Kotygor\Mylog;
 
 class OpenCartExtensionInstallerPlugin implements PluginInterface
 {
@@ -12,18 +13,18 @@ class OpenCartExtensionInstallerPlugin implements PluginInterface
 
     public function activate(Composer $composer, IOInterface $io)
     {
-    	$this->io->write("opencart-extension-installer <info>activate method</info>");
+    	$io->write("opencart-extension-installer <info>activate method</info>");
         $installer = new OpenCartExtensionInstaller($io, $composer, $this->packageType);
         $composer->getInstallationManager()->addInstaller($installer);
     }
 
 	public function deactivate(Composer $composer, IOInterface $io) {
-		$this->io->write("opencart-extension-installer <info>deactivate method</info>");
+		$io->write("opencart-extension-installer <info>deactivate method</info>");
 		$installer = new OpenCartExtensionInstaller($io, $composer, $this->packageType);
 		$composer->getInstallationManager()->removeInstaller($installer);
 	}
 	public function uninstall(Composer $composer, IOInterface $io) {
-		$this->io->write("opencart-extension-installer <info>uninstall method</info>");
+		$io->write("opencart-extension-installer <info>uninstall method</info>");
 		$installer = new OpenCartExtensionInstaller($io, $composer, $this->packageType);
 		$composer->getInstallationManager()->removeInstaller($installer);
 	}
