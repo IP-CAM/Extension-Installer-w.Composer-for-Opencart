beaae4229456d57451c0e0dd86f8a04a5d2021ff
JD <zelenuj@gmail.com>
2020-10-29 15:56:29 +0200

    Вияснення як працює uninstall

--- src/BeyondIT/Composer/OpenCartExtensionInstallerPlugin.php
+++ src/BeyondIT/Composer/OpenCartExtensionInstallerPlugin.php
@@ -12,16 +12,19 @@ class OpenCartExtensionInstallerPlugin implements PluginInterface
 
     public function activate(Composer $composer, IOInterface $io)
     {
+    	$this->io->write("opencart-extension-installer <info>activate method</info>");
         $installer = new OpenCartExtensionInstaller($io, $composer, $this->packageType);
         $composer->getInstallationManager()->addInstaller($installer);
     }
 
 	public function deactivate(Composer $composer, IOInterface $io) {
+		$this->io->write("opencart-extension-installer <info>deactivate method</info>");
 		$installer = new OpenCartExtensionInstaller($io, $composer, $this->packageType);
-		$composer->getInstallationManager()->addInstaller($installer);
+		$composer->getInstallationManager()->removeInstaller($installer);
 	}
 	public function uninstall(Composer $composer, IOInterface $io) {
+		$this->io->write("opencart-extension-installer <info>uninstall method</info>");
 		$installer = new OpenCartExtensionInstaller($io, $composer, $this->packageType);
-		$composer->getInstallationManager()->addInstaller($installer);
+		$composer->getInstallationManager()->removeInstaller($installer);
 	}
 }
