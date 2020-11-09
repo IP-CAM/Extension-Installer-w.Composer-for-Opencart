d71a1c0d6326058ade763638f94d2823b3887b09
JD <zelenuj@gmail.com>
2020-11-09 14:07:31 +0200

    Fatal error: Cannot redeclare modification() (previously declared in startup.php) Виникає при встановленні 2-го і наступних модулів ()

--- src/BeyondIT/Composer/OpenCartExtensionInstaller.php
+++ src/BeyondIT/Composer/OpenCartExtensionInstaller.php
@@ -113,23 +113,24 @@ class OpenCartExtensionInstaller extends LibraryInstaller
 
         // only trigger install iff config is available
         if (is_file('admin/config.php')) {
-	        $_SERVER['SERVER_PORT'] = 80;
-	        $_SERVER['SERVER_PROTOCOL'] = 'CLI';
-	        $_SERVER['REQUEST_METHOD'] = 'GET';
-	        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+        	if(!function_exists('modification')) {
+		        $_SERVER['SERVER_PORT'] = 80;
+		        $_SERVER['SERVER_PROTOCOL'] = 'CLI';
+		        $_SERVER['REQUEST_METHOD'] = 'GET';
+		        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 
-	        ob_start();
-	            require_once('admin/config.php');
-	            $application_config = 'admin';
-	            include('system/startup.php');
-	            include ('system/framework.php');
-	        ob_end_clean();
+		        ob_start();
+		        require_once('admin/config.php');
+		        $application_config = 'admin';
+		        include('system/startup.php');
+		        include('system/framework.php');
+		        ob_end_clean();
 
-	        chdir($tmpDir);
+		        chdir($tmpDir);
 
-            // $registry comes from system/framework.php
-	        OpenCartNaivePhpInstaller::$registry = $registry;
-
+		        // $registry comes from system/framework.php
+		        OpenCartNaivePhpInstaller::$registry = $registry;
+	        }
 	        $installer = new OpenCartNaivePhpInstaller();
 	        $installer->install($file);
 	    }
